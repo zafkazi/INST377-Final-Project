701253ab0e4d058ca34d4af308250ecf62a8c6bb
Zafir Kazi <zkazi02@terpmail.umd.edu>
2025-05-17 12:44:20 -0400

    new

--- final_project.js
+++ final_project.js
@@ -7,4 +7,39 @@ function textValidation(){
                 return false;
             }
             return true;
+}
+
+//populate currencies in form
+function populateForm(){
+    const fromCurrency = document.getElementById("fromCurrency");
+    const toCurrency  = document.getElementById("toCurrency");
+
+
+    fetch("https://api.frankfurter.app/currencies")
+    .then((res) => res.json())
+    .then((resJson) => {
+        console.log('Response JSON: ', resJson);
+
+        for (const currencyCode in resJson) {
+            console.log('Object: ', currencyCode);
+            
+            //Populate From Currency
+            const option = document.createElement("option");
+            option.value = currencyCode;
+            option.innerHTML = resJson[currencyCode];
+            fromCurrency.appendChild(option);
+            
+            //Populate To Currency
+            const option2 = document.createElement("option");
+            option2.value = currencyCode;
+            option2.innerHTML = resJson[currencyCode];
+            toCurrency.appendChild(option2);
         }
+    });
+
+}
+
+
+
+
+window.onload = populateForm;
